797d5ae475cf358346b30b413f1ab12dc8ff8a44
Roman Vavrynkevych <roman.vavrun@gmail.com>
2020-01-22 06:26:23 +0200

    edit fn almost ready

--- script_3.js
+++ script_3.js
@@ -5,48 +5,98 @@
 // * Реалізувати можливість виділяти декілька рядків і видаляти їх.
 'use strict';
 
-// // body node
-// let body = document.body;
-// body.style.margin = '0'; // body no margin
-
-// // wrapper
-// let wrapper = document.createElement('div');
-// // wrapper full screan size
-// wrapper.style.width = '100vw';
-// wrapper.style.height = '100vh';
-// // flex align all elem in center
-// wrapper.style.display = 'flex';
-// wrapper.style.justifyContent = 'center';
-// wrapper.style.alignItems = 'center';
-// // else styles
-// wrapper.style.backgroundColor = '#FFF';
-// wrapper.style.position = 'relative';
-
-// // add wrapper to body
-// body.append(wrapper);
-
-// // wrapper node 
-// let container = body.lastChild;
-
+// Time -----
 // get current time
 let today = new Date();
-let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-let dateTime = date + ' ' + time;
+let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate(); // Y-M-D
+let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(); // H-M-S
+let dateTime = date + ' ' + time; // full time Y-M-D - H-M-S
 
+// add time to table 
 let tableDate = document.getElementById('dateTime');
 tableDate.innerHTML = dateTime;
+// Time -----
 
-let btnColection = document.getElementsByClassName('delete');
-let delBrn = [...btnColection];
-
-
-delBrn.map((item) => {
-    console.log(item)
+// Delete fn -----
+//  get all delete buttons
+let btnDeleteColection = document.getElementsByClassName('delete');
+// convert from htmlcolection to array
+let delBtn = [...btnDeleteColection];
+// add listener for delete btn
+delBtn.map((item) => {
     item.addEventListener('click', deleteRow)
 })
 
+//  delete row fn
 function deleteRow(item) {
     item.path[2].remove()
 }
-// console.log(delBrn)
+// Delete fn -----
+
+// edit row -----
+//  get all edit buttons
+let btnEditColection = document.getElementsByClassName('editbtn');
+// convert from htmlcolection to array
+let editBtn = [...btnEditColection];
+// add listener for edit btn
+editBtn.map((item) => {
+    item.addEventListener('click', editRow)
+})
+
+//  edit row fn
+function editRow(item) {
+    // remember key/value
+    let elem_key = item.path[2].cells[0] // Key
+    let elem_value = item.path[2].cells[1] // Value
+
+    // remember key/value text
+    let elem_key_text = elem_key.innerHTML;
+    let elem_value_text = elem_value.innerHTML;
+
+    // edit button disabled on
+    let currentBtn = item.target;
+    currentBtn.setAttribute('disabled', '');
+
+    // create inputs to add inside 
+    let editInputKey = document.createElement('input');
+    editInputKey.setAttribute('type', 'text');
+    editInputKey.setAttribute('class', 'editing');
+    editInputKey.setAttribute('value', elem_key_text);
+
+    let editInputValue = document.createElement('input');
+    editInputValue.setAttribute('type', 'text');
+    editInputValue.setAttribute('class', 'editing');
+    editInputValue.setAttribute('value', elem_value_text);
+
+    // add event listener
+    editInputKey.addEventListener('keypress', finEditing);
+    editInputValue.addEventListener('keypress', finEditing);
+
+    // finishing editing
+    function finEditing(item) {
+        if (item.key === 'Enter' && item.target.value.trim() !== '') {
+
+            //  new text
+            let newText = item.target.value;
+
+            // add new text
+            item.path[1].innerHTML = newText;
+        }
+
+        //  es
+        if (item.path[2].cells[1].innerHTML.length < 40 && item.path[2].cells[0].innerHTML.length < 40) {
+            // edit button disabled off
+            currentBtn.removeAttribute('disabled')
+        }
+
+    }
+
+    // reset key/value text
+    elem_key.innerHTML = '';
+    elem_value.innerHTML = '';
+
+    // add inputs
+    elem_key.append(editInputKey);
+    elem_value.append(editInputValue);
+}
+// edit row -----
